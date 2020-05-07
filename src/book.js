6f11bfb164daee74e803bf2cc222fcad3be7821c
Jordy Shryock <jordy1611@gmail.com>
2020-05-07 16:46:54 -0600

    Finished book testing

--- src/book.js
+++ src/book.js
@@ -1,9 +1,48 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  return `The ${title}`;
+};
+
+function buildMainCharacter(charName, charAge, charPronouns) {
+  return character = {
+    name: charName,
+    age: charAge,
+    pronouns: charPronouns,
+  }
+};
+
+function saveReview(review, reviews) {
+  if (reviews.includes(review)) {
+    return reviews
+  } else {
+    return reviews.push(review);
+  }
+};
+
+function calculatePageCount(bookTitle) {
+  bookPageCount = bookTitle.length * 20;
+  return bookPageCount
+};
+
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: bookGenre,
+  }
+};
+
+function editBook() {
+book.pageCount = book.pageCount*.75
+};
 
 module.exports = {
   createTitle: createTitle,
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook,
   // buildMainCharacter: buildMainCharacter,
   // saveReview: saveReview,
   // calculatePageCount: calculatePageCount,
