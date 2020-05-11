0d009090a5c3f78496b03ca83686e6d3683eca7b
Jordy Shryock <jordy1611@gmail.com>
2020-05-11 08:52:11 -0600

    Initial Commit

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,20 @@
 
+function createLibrary(library) {
+cityLibrary = {
+  name: library,
+shelves: {fantasy: [], fiction: [], nonFiction: []}
+};
+return cityLibrary;
+};
+
+function addBook(cityLibrary, book) {
+  if (book.genre === "fantasy") {
+  cityLibrary.shelves.fantasy.push(book);
+};
+};
 
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
+createLibrary: createLibrary,
+addBook: addBook,
   // checkoutBook: checkoutBook
 };
